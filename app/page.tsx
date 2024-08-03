ad3cc4d269d184f73d4f9a62f9264274b76221aa
kohei240155 <k.pino519@gmail.com>
2024-08-03 21:18:33 +0900

    不要なコードを削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,13 @@
 "use client"
-import { useEffect, useState } from "react";
 import styles from "./page.module.css";
-import LoginButton from "./components/elements/button/LoginButton";
 import * as Deck from "../features/deck/Index";
-import Header from "./components/layouts/header/Header";
 import Footer from "./components/layouts/footer/Footer";
 
 export default function Home() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setLoggedIn(true);
-  }
 
   return (
     <div className={styles.container}>
-      {/* <Header /> */}
       <main className={styles.main}>
-        {/* {loggedIn ? (
-          <Deck.DeckList />
-        ) : (
-          <LoginButton onLogin={handleLogin} />
-        )} */}
         <Deck.DeckList />
       </main>
       <Footer />
