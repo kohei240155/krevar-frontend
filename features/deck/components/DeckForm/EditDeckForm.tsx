b7e899b846f4934fd8263903631dac1dec61dcca
kohei240155 <k.pino519@gmail.com>
2024-08-10 17:34:16 +0900

    ボタンの間隔を修正

--- features/deck/components/DeckForm/EditDeckForm.tsx
+++ features/deck/components/DeckForm/EditDeckForm.tsx
@@ -50,19 +50,21 @@ const EditDeckForm: React.FC<DeckSettingsProps> = ({ deckId, deckName: initialDe
                         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
                     />
                 </div>
+                <div className="mb-2">
+                    <button
+                        type="submit"
+                        className="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Update
+                    </button>
+                </div>
                 <button
-                    type="submit"
-                    className="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    onClick={handleDelete}
+                    className="w-full inline-flex items-center justify-center px-4 py-2 border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
                 >
-                    Update
+                    Delete
                 </button>
             </form>
-            <button
-                onClick={handleDelete}
-                className="w-full inline-flex items-center justify-center px-4 py-2 border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 mt-4"
-            >
-                Delete
-            </button>
         </div>
     )
 }
