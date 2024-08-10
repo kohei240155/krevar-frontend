c6f8053969be29e2482eac57797feef465acd204
kohei240155 <k.pino519@gmail.com>
2024-08-10 11:37:27 +0900

    Editボタンの形を変更

--- features/word/components/WordList/WordList.tsx
+++ features/word/components/WordList/WordList.tsx
@@ -58,7 +58,7 @@ const WordList: React.FC<WordListProps> = ({ deckId }) => {
                 </span>
               </div>
               <button
-                className="text-blue-500 hover:underline"
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-700 transition"
                 onClick={() => handleEditClick(word.id)}
               >
                 Edit
