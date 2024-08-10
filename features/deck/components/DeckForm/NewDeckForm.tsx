f88f52bf42d928a99ef29a90e8b8b889a76f2ffe
kohei240155 <k.pino519@gmail.com>
2024-08-10 17:16:58 +0900

    Cancelボタンの色を修正

--- features/deck/components/DeckForm/NewDeckForm.tsx
+++ features/deck/components/DeckForm/NewDeckForm.tsx
@@ -46,7 +46,7 @@ const DeckForm: React.FC<DeckFormProps> = ({ onDeckCreated }) => {
                 </div>
                 <button
                     onClick={onDeckCreated}
-                    className="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                    className="w-full inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Cancel
                 </button>
