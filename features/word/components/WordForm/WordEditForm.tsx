2ef5f66f25ed633c0273fcebdce2efc7b8ff0b32
kohei240155 <k.pino519@gmail.com>
2024-08-11 14:29:18 +0900

    backwordボタンを押下したときの遷移先を変更

--- features/word/components/WordForm/WordEditForm.tsx
+++ features/word/components/WordForm/WordEditForm.tsx
@@ -100,7 +100,7 @@ const WordEditForm: React.FC<WordEditFormProps> = ({ wordId }) => {
         </div>
         <button
           type="button"
-          onClick={() => router.push('/')}
+          onClick={() => router.back()}
           className="w-full inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           backward
