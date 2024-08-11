aa7c172d243596fe9ed3486039d755a1798b7e3b
kohei240155 <k.pino519@gmail.com>
2024-08-11 12:46:18 +0900

    不要文言を削除

--- app/words/new/page.tsx
+++ app/words/new/page.tsx
@@ -4,7 +4,6 @@ import * as Word from "../../../features/word/components/index";
 const NewWord = () => {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4 text-center">New Word</h1>
       <Word.WordForm />
     </div>
   )
