af1bc2cc953f5b415f69fcea1cb4fbe155577960
kohei240155 <k.pino519@gmail.com>
2024-08-11 14:55:48 +0900

    ボタンの配置を変更

--- features/deck/components/DeckForm/EditDeckForm.tsx
+++ features/deck/components/DeckForm/EditDeckForm.tsx
@@ -73,21 +73,21 @@ const EditDeckForm: React.FC<DeckSettingsProps> = ({ deckId, deckName: initialDe
                         className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
                     />
                 </div>
-                <div className="mb-2">
+                <div className="flex justify-between mb-2">
+                    <button
+                        type="button"
+                        onClick={() => router.push('/')}
+                        className="w-1/2 mr-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    >
+                        Backward
+                    </button>
                     <button
                         type="submit"
-                        className="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="w-1/2 ml-2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         Update
                     </button>
                 </div>
-                <button
-                    type="button"
-                    onClick={() => router.push('/')}
-                    className="w-full inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    backward
-                </button>
             </form>
 
             <Modal
