020c1e997fc63ccdff471de601fef7c18cc48277
kohei240155 <k.pino519@gmail.com>
2024-08-11 18:46:35 +0900

    ボタンのレイアウトを変更

--- features/word/components/WordList/WordList.tsx
+++ features/word/components/WordList/WordList.tsx
@@ -54,16 +54,16 @@ const WordList: React.FC<WordListProps> = ({ deckId }) => {
       <div className="p-4">
         <div className="max-w-2xl mx-auto mt-1 p-6 bg-white rounded-lg shadow-md">
           <h2 className="text-2xl font-bold mb-4 text-left">{deckName}</h2>
-          <p className="text-gray-500 text-center mt-4">No words to display.</p>
-          <div className="flex space-x-4">
+          <p className="text-gray-500 text-center mt-4 text-xl">No words to display.</p>
+          <div className="flex justify-between mt-10">
             <button
-              className="w-full mt-4 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-1/2 mr-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={handleBackClick}
             >
               Backward
             </button>
             <button
-              className="w-full mt-4 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-1/2 ml-2 inline-flex items-center justify-center px-4 py-2 border border-white text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               onClick={handleAddWordClick}
             >
               Add Word
