34d4bc27b7c747793865356bff9b771e7fb2fd6b
kohei240155 <k.pino519@gmail.com>
2024-08-11 18:08:46 +0900

    Backwordボタンを追加

--- features/word/components/WordForm/WordForm.tsx
+++ features/word/components/WordForm/WordForm.tsx
@@ -100,13 +100,24 @@ const WordForm = () => {
         </div>
         {/* Image generate ボタン */}
         <div className="mb-4">
-          <button
-            type="button"
-            onClick={handleImageGenerate}
-            className="w-full inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-transform transform hover:scale-105"
-          >
-            Image generate
-          </button>
+          {!isImageGenerated && (
+            <>
+              <button
+                type="button"
+                onClick={handleImageGenerate}
+                className="w-full inline-flex items-center justify-center px-4 py-2 border border-white text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-transform transform hover:scale-105"
+              >
+                Image generate
+              </button>
+              <button
+                type="button"
+                onClick={() => router.back()}
+                className="w-full mt-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Backward
+              </button>
+            </>
+          )}
         </div>
         {isImageGenerated && (
           <>
