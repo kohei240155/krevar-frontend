18d609c90cb85c09f7082a1de7bd7028d9b63f1f
kohei240155 <k.pino519@gmail.com>
2024-08-11 13:56:12 +0900

    Cancelをbackwardに変更

--- features/deck/components/DeckForm/NewDeckForm.tsx
+++ features/deck/components/DeckForm/NewDeckForm.tsx
@@ -58,7 +58,7 @@ const DeckForm: React.FC<DeckFormProps> = ({ onDeckCreated }) => {
                     onClick={() => router.push('/')}
                     className="w-full inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                    Cancel
+                    backward
                 </button>
             </form>
         </div>
