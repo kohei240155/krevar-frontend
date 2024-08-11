1af2558b36ab26ec848c75f1cb816a7b243057ec
kohei240155 <k.pino519@gmail.com>
2024-08-11 14:54:16 +0900

    ボタンの配置を変更

--- features/word/components/WordForm/WordEditForm.tsx
+++ features/word/components/WordForm/WordEditForm.tsx
@@ -117,21 +117,21 @@ const WordEditForm: React.FC<WordEditFormProps> = ({ wordId }) => {
             className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
           />
         </div>
-        <div className="mb-2">
+        <div className="flex justify-between mb-2">
+          <button
+            type="button"
+            onClick={() => router.back()}
+            className="w-1/2 mr-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            backward
+          </button>
           <button
             type="submit"
-            className="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            className="w-1/2 ml-2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
             Update
           </button>
         </div>
-        <button
-          type="button"
-          onClick={() => router.back()}
-          className="w-full inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          backward
-        </button>
       </form>
 
       <Modal
