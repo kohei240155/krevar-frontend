63d905a83519c6820a1902a0c0a746af6b57f3aa
kohei240155 <k.pino519@gmail.com>
2024-08-11 13:32:00 +0900

    Cancelボタン押下時の挙動を修正

--- features/deck/components/DeckForm/NewDeckForm.tsx
+++ features/deck/components/DeckForm/NewDeckForm.tsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useRouter } from 'next/navigation';
 
 interface DeckFormProps {
     onDeckCreated: () => void;
@@ -10,6 +11,7 @@ interface DeckFormProps {
 
 const DeckForm: React.FC<DeckFormProps> = ({ onDeckCreated }) => {
     const [deckName, setDeckName] = useState('');
+    const router = useRouter();
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
@@ -45,13 +47,15 @@ const DeckForm: React.FC<DeckFormProps> = ({ onDeckCreated }) => {
                 <div className="mb-2">
                     <button
                         type="submit"
+                        onClick={onDeckCreated}
                         className="w-full inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         Register
                     </button>
                 </div>
                 <button
-                    onClick={onDeckCreated}
+                    type="button"
+                    onClick={() => router.push('/')}
                     className="w-full inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Cancel
