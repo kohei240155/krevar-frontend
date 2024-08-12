93ee649f2deade1190ea7163d46e0f1339e9f84d
kohei240155 <k.pino519@gmail.com>
2024-08-12 21:28:18 +0900

    ログインのボタン回りを修正

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -1,32 +1,15 @@
 "use client";
 
-import Image from "next/image";
+import { IoPerson } from "react-icons/io5";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import Link from "next/link";
 import { type Session } from "next-auth";
-import { signOut } from "next-auth/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const Header = ({ session }: { session: Session | null }) => {
-  // const [isFixed, setIsFixed] = useState(true);
-
-  // useEffect(() => {
-  //   const handleScroll = () => {
-  //     const footer = document.getElementById('footer');
-  //     if (footer) {
-  //       const footerRect = footer.getBoundingClientRect();
-  //       const isFooterVisible = footerRect.top < window.innerHeight;
-  //       setIsFixed(!isFooterVisible);
-  //     }
-  //   };
-
-  //   window.addEventListener('scroll', handleScroll);
-  //   return () => {
-  //     window.removeEventListener('scroll', handleScroll);
-  //   }
-  // }, []);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
-    // <header className={`flex items-center justify-between bg-white p-4 shadow-md ${isFixed ? 'fixed top-0 w-full z-10' : ''}`}>
     <header className="flex items-center justify-between bg-white p-4 shadow-md">
       <div className="flex items-center">
         <Link href="/" className="text-4xl font-bold">
@@ -50,25 +33,13 @@ const Header = ({ session }: { session: Session | null }) => {
           </Link>
         </li>
         {session ? (
-          <>
-            <li>
-              <Image
-                src={session.user?.image ?? ""}
-                alt={session.user?.name ?? ""}
-                width={40}
-                height={40}
-                className="rounded-full"
-              />
-            </li>
-            <li>
-              <button
-                onClick={() => signOut()}
-                className="rounded-lg bg-blue-600 px-4 py-[7px] text-white hover:bg-gray-600"
-              >
-                Logout
-              </button>
-            </li>
-          </>
+          <li className="flex items-center space-x-2">
+            <IoPerson className="text-2xl" />
+            <span>{session.user?.name}</span>
+            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+              {isDropdownOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+            </button>
+          </li>
         ) : (
           <li>
             <Link href="/login">
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { useSession, signIn } from "next-auth/react";
 import { useEffect } from "react";
 import styles from "./page.module.css";
 import Decks from "./decks/page";
+import Header from "./components/layouts/header/Header";
 
 export default function Home() {
   const { data: session, status } = useSession();
@@ -11,23 +12,18 @@ export default function Home() {
     return <div>Loading...</div>; // 認証状態を確認中のローディング表示
   }
 
-  if (status === "authenticated") {
-    return (
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <Decks />
-        </main>
-      </div>
-    );
-  }
-
-  // ログアウト状態ではTop画面を表示
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>Welcome to IRUKA - Vocabulary Memorization App</h1>
-        <p>Please log in to view your decks.</p>
-        <button onClick={() => signIn("google")}>Log in with Google</button>
+        {status === "authenticated" ? (
+          <Decks />
+        ) : (
+          <>
+            <h1>Welcome to IRUKA - Vocabulary Memorization App</h1>
+            <p>Please log in to view your decks.</p>
+            <button onClick={() => signIn("google")}>Log in with Google</button>
+          </>
+        )}
       </main>
     </div>
   );
