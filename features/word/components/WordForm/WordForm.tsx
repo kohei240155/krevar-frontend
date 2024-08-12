05e093455f95d0a99cbb237bf658f1e24ebcbc96
kohei240155 <k.pino519@gmail.com>
2024-08-12 19:42:39 +0900

    Image generateボタンの装飾を変更

--- features/word/components/WordForm/WordForm.tsx
+++ features/word/components/WordForm/WordForm.tsx
@@ -130,14 +130,14 @@ const WordForm = () => {
               <button
                 type="button"
                 onClick={handleImageGenerate}
-                className="w-full inline-flex items-center justify-center px-4 py-2 border border-white text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 transition-transform transform hover:scale-105"
+                className="w-full inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 transition duration-300 ease-in-out transform hover:scale-105"
               >
                 Image generate
               </button>
               <button
                 type="button"
                 onClick={() => router.back()}
-                className="w-full mt-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="w-full mt-3 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Backward
               </button>
