bb18c5e67ba748eb6426304427a02184c5e82e8e
kohei240155 <k.pino519@gmail.com>
2024-08-15 00:17:07 +0900

    ハンバーガーメニューを修正

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -52,6 +52,19 @@ const Header = ({ session }: { session: Session | null }) => {
               <Link href="/statistic" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Statistic</Link>
             </li>
             {session && (
+              <>
+                <li className="block md:hidden">
+                  <Link href="/information" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Information</Link>
+                </li>
+                <li className="block md:hidden">
+                  <Link href="/user-settings" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">User Settings</Link>
+                </li>
+                <li className="block md:hidden">
+                  <button onClick={handleLogout} className="block w-full py-2 px-3 text-left text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Logout</button>
+                </li>
+              </>
+            )}
+            {session && (
               <li className="relative items-center space-x-2 dropdown hidden md:flex">
                 <IoPerson className="text-2xl" />
                 <span>{session.user?.name}</span>
