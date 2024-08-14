23fa4ddc4acf2986245cafbd755c80a94e4f6159
kohei240155 <k.pino519@gmail.com>
2024-08-15 00:11:26 +0900

    ハンバーガーメニューを修正

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -51,8 +51,8 @@ const Header = ({ session }: { session: Session | null }) => {
             <li>
               <Link href="/statistic" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Statistic</Link>
             </li>
-            {session ? (
-              <li className="relative flex items-center space-x-2 dropdown">
+            {session && (
+              <li className="relative items-center space-x-2 dropdown hidden md:flex">
                 <IoPerson className="text-2xl" />
                 <span>{session.user?.name}</span>
                 <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
@@ -76,7 +76,8 @@ const Header = ({ session }: { session: Session | null }) => {
                   </ul>
                 )}
               </li>
-            ) : (
+            )}
+            {!session && !isMenuOpen && (
               <li>
                 <Link href="/login">
                   <button className="rounded-lg bg-blue-600 px-4 py-[7px] text-white hover:bg-blue-700">
