0a38f6e2a0816bd1755588a82d480f7ab39fb8a2
kohei240155 <k.pino519@gmail.com>
2024-08-15 00:23:07 +0900

    ログインボタンの高さを修正

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -30,7 +30,7 @@ const Header = ({ session }: { session: Session | null }) => {
 
   return (
     <header className="bg-white border-gray-200 dark:bg-gray-900">
-      <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4">
+      <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4 h-16">
         <Link href="/" className="text-4xl font-bold ml-0">
           ▶ IRUKA
         </Link>
@@ -41,7 +41,7 @@ const Header = ({ session }: { session: Session | null }) => {
           </svg>
         </button>
         <div className={`${isMenuOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-multi-level">
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 items-center">
             <li>
               <Link href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Deck List</Link>
             </li>
