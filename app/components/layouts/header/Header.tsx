a0fbf2e7de4e2e45c049a55a90ca1d4035e4c187
kohei240155 <k.pino519@gmail.com>
2024-08-14 21:34:09 +0900

    ヘッダーのレスポンシブを修正

--- app/components/layouts/header/Header.tsx
+++ app/components/layouts/header/Header.tsx
@@ -29,66 +29,65 @@ const Header = ({ session }: { session: Session | null }) => {
   }, [isDropdownOpen]);
 
   return (
-    <header className="flex items-center justify-between bg-white p-4 shadow-md">
-      <div className="flex items-center">
-        <Link href="/" className="text-4xl font-bold">
+    <header className="bg-white border-gray-200 dark:bg-gray-900">
+      <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link href="/" className="text-4xl font-bold ml-0">
           ▶ IRUKA
         </Link>
-      </div>
-      <button className="md:hidden" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-        ☰
-      </button>
-      <ul className={`flex-col md:flex-row md:flex items-center space-x-8 ${isMenuOpen ? 'flex' : 'hidden'}`}>
-        <li className="hover:text-blue-600 transition-colors duration-300">
-          <Link href="/" className="text-lg">
-            Deck List
-          </Link>
-        </li>
-        <li className="hover:text-blue-600 transition-colors duration-300">
-          <Link href="/decks/new" className="text-lg">
-            Add Deck
-          </Link>
-        </li>
-        <li className="hover:text-blue-600 transition-colors duration-300">
-          <Link href="/statistic" className="text-lg">
-            Statistic
-          </Link>
-        </li>
-        {session ? (
-          <li className="relative flex items-center space-x-2 dropdown">
-            <IoPerson className="text-2xl" />
-            <span>{session.user?.name}</span>
-            <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
-              {isDropdownOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
-            </button>
-            {isDropdownOpen && (
-              <ul className="absolute right-0 mt-40 w-48 bg-white shadow-lg dropdown">
-                <li className="hover:bg-gray-100 transition-colors duration-300">
-                  <button
-                    onClick={handleLogout}
-                    className="block w-full px-4 py-2 text-left"
-                  >
-                    Logout
+        <button data-collapse-toggle="navbar-multi-level" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-multi-level" aria-expanded="false">
+          <span className="sr-only">Open main menu</span>
+          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          </svg>
+        </button>
+        <div className="hidden w-full md:block md:w-auto" id="navbar-multi-level">
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <Link href="/" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Deck List</Link>
+            </li>
+            <li>
+              <Link href="/decks/new" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Add Deck</Link>
+            </li>
+            <li>
+              <Link href="/statistic" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Statistic</Link>
+            </li>
+            {session ? (
+              <li className="relative flex items-center space-x-2 dropdown">
+                <IoPerson className="text-2xl" />
+                <span>{session.user?.name}</span>
+                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)}>
+                  {isDropdownOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                </button>
+                {isDropdownOpen && (
+                  <ul className="absolute right-0 mt-40 w-48 bg-white shadow-lg dropdown">
+                    <li className="hover:bg-gray-100 transition-colors duration-300">
+                      <button
+                        onClick={handleLogout}
+                        className="block w-full px-4 py-2 text-left"
+                      >
+                        Logout
+                      </button>
+                    </li>
+                    <li className="hover:bg-gray-100 transition-colors duration-300">
+                      <Link href="/information" className="block w-full px-4 py-2 text-left">
+                        Information
+                      </Link>
+                    </li>
+                  </ul>
+                )}
+              </li>
+            ) : (
+              <li>
+                <Link href="/login">
+                  <button className="rounded-lg bg-blue-600 px-4 py-[7px] text-white hover:bg-blue-700">
+                    Login
                   </button>
-                </li>
-                <li className="hover:bg-gray-100 transition-colors duration-300">
-                  <Link href="/information" className="block w-full px-4 py-2 text-left">
-                    Information
-                  </Link>
-                </li>
-              </ul>
+                </Link>
+              </li>
             )}
-          </li>
-        ) : (
-          <li>
-            <Link href="/login">
-              <button className="rounded-lg bg-blue-600 px-4 py-[7px] text-white hover:bg-blue-700">
-                Login
-              </button>
-            </Link>
-          </li>
-        )}
-      </ul>
+          </ul>
+        </div>
+      </div>
     </header>
   );
 };
