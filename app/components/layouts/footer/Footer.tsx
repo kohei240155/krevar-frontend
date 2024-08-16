6f9563bd75ddfff939757f1c2ec996f34c0cc453
kohei240155 <k.pino519@gmail.com>
2024-08-17 00:08:29 +0900

    リンクを追加

--- app/components/layouts/footer/Footer.tsx
+++ app/components/layouts/footer/Footer.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
     return (
         <footer id="footer" className="bg-gray-800 text-white py-2">
             <div className="w-full px-2 flex justify-between items-center">
                 <div className="flex space-x-4">
-                    <a href="#" className="text-gray-400 hover:text-white">About</a>
-                    <a href="#" className="text-gray-400 hover:text-white">Privacy</a>
-                    <a href="#" className="text-gray-400 hover:text-white">Terms</a>
-                    <a href="#" className="text-gray-400 hover:text-white">Support</a>
+                    <Link href="/about" legacyBehavior>
+                        <a className="text-gray-400 hover:text-white">About</a>
+                    </Link>
+                    <Link href="/privacy" legacyBehavior>
+                        <a className="text-gray-400 hover:text-white">Privacy</a>
+                    </Link>
+                    <Link href="/terms" legacyBehavior>
+                        <a className="text-gray-400 hover:text-white">Terms</a>
+                    </Link>
+                    <Link href="/support" legacyBehavior>
+                        <a className="text-gray-400 hover:text-white">Support</a>
+                    </Link>
                 </div>
                 <div>
                     <span className="text-gray-400 text-xs">© 2024 IRUKA.</span>
