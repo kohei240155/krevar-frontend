e91ed19bf4d083180ca4891de471ab7abaddd861
kohei240155 <k.pino519@gmail.com>
2024-08-17 00:27:26 +0900

    画面更新時にTOP画面が表示されないように修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 "use client"
 import { useSession, signIn } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./page.module.css";
 import Decks from "./decks/page";
 import Header from "./components/layouts/header/Header";
 
 export default function Home() {
   const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "authenticated") {
+      router.push("/decks");
+    }
+  }, [status, router]);
 
   return (
     <div className={styles.container}>
