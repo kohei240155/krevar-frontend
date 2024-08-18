8757f5cb006786515d7b6f6dcbbb555aca8bca64
kohei240155 <k.pino519@gmail.com>
2024-08-18 21:05:28 +0900

    レイアウトを調整

--- features/word/components/WordForm/WordForm.tsx
+++ features/word/components/WordForm/WordForm.tsx
@@ -179,7 +179,7 @@ const WordForm = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md">
+    <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md">
       <h1 className="text-2xl font-bold mb-6 text-left">Add Word</h1>
       <form onSubmit={handleSubmit}>
         {/* 単語を入力る欄 */}
