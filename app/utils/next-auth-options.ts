4b1cc1eaf626c69539939f3265436d6fbb77c33b
kohei240155 <k.pino519@gmail.com>
2024-08-24 20:19:38 +0900

    ログインが成功するよう修正

--- app/utils/next-auth-options.ts
+++ app/utils/next-auth-options.ts
@@ -6,7 +6,7 @@ import type { NextAuthOptions } from "next-auth";
 
 export const nextAuthOptions: NextAuthOptions = {
   debug: true, // デバッグモードを有効にする
-  session: { strategy: "jwt" },
+  session: { strategy: "database" }, // セッション方式に変更
   providers: [
     // TwitterProvider({
     //   clientId: process.env.TWITTER_CLIENT_ID!,
@@ -51,22 +51,8 @@ export const nextAuthOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    jwt: async ({ token, user, account, profile }) => {
-      // 注意: トークンをログ出力してはダメです。
-      // console.log("in jwt", { user, token, account, profile });
-      if (user) {
-        token.user = user;
-        const u = user as any;
-        token.role = u.role;
-      }
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
     session: ({ session, token }) => {
-      // console.log("in session", { session, token });
-      token.accessToken;
+      console.log("in session", { session, token });
       return {
         ...session,
         user: {
