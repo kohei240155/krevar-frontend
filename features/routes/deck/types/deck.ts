ad19f33cffe0a4a1e0b4f126b84607fe8891c18c
kohei240155 <k.pino519@gmail.com>
2024-08-31 12:36:02 +0900

    型定義を追加

--- features/routes/deck/types/deck.ts
+++ features/routes/deck/types/deck.ts
@@ -3,3 +3,10 @@ export interface Deck {
     deckName: string;
     totalQuestions: number;
 }
+export interface DeckItemProps {
+    deck: Deck;
+    showOptions: number | null;
+    onQuizClick: (deckId: number, deckName: string) => void;
+    onOptionClick: (e: React.MouseEvent, deckId: number) => void;
+    onOptionItemClick: (e: React.MouseEvent, option: string, deck: Deck) => void;
+}
