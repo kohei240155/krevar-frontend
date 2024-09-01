edf94e6eb25597f7cab13d8f0810f3ab332887fd
kohei240155 <k.pino519@gmail.com>
2024-09-01 12:09:14 +0900

    お問い合わせページを作成

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const ContactPage = () => {
+  return (
+    <div className="p-5">
+      <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md">
+        <h1 className="text-xl font-semibold text-center mb-4 text-gray-800">
+          お問い合わせ
+        </h1>
+        <p className="mb-4 text-sm text-gray-600">
+          IRUKAをご利用いただきありがとうございます。
+        </p>
+        <p className="mb-4 text-sm text-gray-600">
+          IRUKAに質問されたいことなどがございましたら下記のメールアドレスまでお問い合わせください。
+        </p>
+        <p className="mb-4 text-sm text-gray-600">
+          Email:{" "}
+          <a
+            href="mailto:iruka.com@gmail.com"
+            className="text-blue-500 hover:underline"
+          >
+            iruka.com@gmail.com
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default ContactPage;
