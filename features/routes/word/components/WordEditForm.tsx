181580cc4da2b7d75c7293ae40e7ecaeb6c51837
kohei240155 <k.pino519@gmail.com>
2024-09-01 12:44:30 +0900

    削除確認モーダルを追加

--- features/routes/word/components/WordEditForm.tsx
+++ features/routes/word/components/WordEditForm.tsx
@@ -5,13 +5,13 @@ import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { FaTrash } from "react-icons/fa";
-import Modal from "react-modal";
 import * as Common from "./../../../common/components/index";
 import { ColorResult } from "react-color";
 import WordInput from "./WordInput";
 import MeaningInput from "./MeaningInput";
 import NuanceInput from "./NuanceInput";
 import ImageDisplay from "./ImageDisplay";
+import DeleteConfirmModal from "./../../../common/components/DeleteConfirmModal";
 
 interface WordEditFormProps {
   wordId: string;
@@ -171,32 +171,12 @@ const WordEditForm: React.FC<WordEditFormProps> = ({ wordId }) => {
           </div>
         </form>
 
-        <Modal
+        <DeleteConfirmModal
           isOpen={isModalOpen}
           onRequestClose={() => setIsModalOpen(false)}
-          contentLabel="Confirm Delete"
-          className="fixed inset-0 flex items-center justify-center p-4 bg-gray-800 bg-opacity-75"
-          overlayClassName="fixed inset-0 bg-black bg-opacity-50"
-        >
-          <div className="bg-white p-6 rounded-lg shadow-lg">
-            <h2 className="text-xl font-bold mb-4">Confirm to delete</h2>
-            <p className="mb-4">Are you sure you want to delete this word?</p>
-            <div className="flex justify-end">
-              <button
-                onClick={() => setIsModalOpen(false)}
-                className="mr-4 px-4 py-2 bg-gray-300 rounded-md"
-              >
-                No
-              </button>
-              <button
-                onClick={confirmDelete}
-                className="px-4 py-2 bg-red-600 text-white rounded-md"
-              >
-                Yes
-              </button>
-            </div>
-          </div>
-        </Modal>
+          onConfirmDelete={confirmDelete}
+          targetWord="word"
+        />
       </div>
     </div>
   );
