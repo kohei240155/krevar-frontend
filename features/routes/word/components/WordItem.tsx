e117aecab70cd4650675860f5dccec22372dbbf9
kohei240155 <k.pino519@gmail.com>
2024-09-01 09:10:40 +0900

    テキストの表示形式を修正

--- features/routes/word/components/WordItem.tsx
+++ features/routes/word/components/WordItem.tsx
@@ -7,7 +7,7 @@ import { WordItemProps } from "../types/word";
     >
       <div className="flex flex-col items-start space-y-2">
         <span className="text-lg font-medium" dangerouslySetInnerHTML={{ __html: word.originalText }} />
-        <span className="text-gray-600" dangerouslySetInnerHTML={{ __html: word.translatedText }} />
+        <span className="text-gray-600">{word.translatedText}</span>
       </div>
       <button
         className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
