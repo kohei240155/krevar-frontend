0bc22165f2d8766e7a96563fd07c12466d7c83a5
kohei240155 <k.pino519@gmail.com>
2024-09-05 22:56:59 +0900

    不要なインポートを削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { getServerSession } from "next-auth";
 import NextAuthProvider from "./providers";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
