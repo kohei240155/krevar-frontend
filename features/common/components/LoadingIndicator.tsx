b09b175570d61a2686e433c2f66c2d0a3efd7000
kohei240155 <k.pino519@gmail.com>
2024-09-12 21:49:06 +0900

    ローディングを修正

--- features/common/components/LoadingIndicator.tsx
+++ features/common/components/LoadingIndicator.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const LoadingIndicator: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="absolute top-0 mt-20 text-xl">Loading...</div>
+    <div className="flex justify-center mt-10">
+      <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
     </div>
   );
 };
