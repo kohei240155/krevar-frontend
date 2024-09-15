41e95915ed83f6306921477eb348d84393e52c44
kohei240155 <k.pino519@gmail.com>
2024-09-15 18:41:57 +0900

    認証方法を修正

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,238 +1,97 @@
 "use client";
 
-import React, { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { signIn, signOut } from "next-auth/react";
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
+import axios from "axios";
 
 const LoginPage = () => {
-  const { data: session, status } = useSession();
+  const { status, data: session } = useSession();
   const router = useRouter();
-  const [isCreatingAccount, setIsCreatingAccount] = useState(false);
 
   useEffect(() => {
-    if (status === "authenticated") {
-      router.push("/deck");
+    // 認証が成功したらJWTが取得できているか確認
+    if (status === "authenticated" && session) {
+      const fetchJWT = async () => {
+        try {
+          const response = await axios.post(
+            "http://localhost:8080/api/auth/google-login",
+            {
+              email: session.user?.email,
+              name: session.user?.name,
+              googleId: session.user?.email, // 'id'を'email'に変更
+            },
+            {
+              headers: { "Content-Type": "application/json" },
+              withCredentials: true,
+            }
+          );
+
+          if (response.status === 200) {
+            const jwtToken = response.data.token; // JWTをレスポンスから取得
+            localStorage.setItem("JWT", jwtToken); // ローカルストレージに保存
+            console.log("ローカルストレージにJWTを保存しました。");
+            router.push("/deck");
+          } else {
+            console.error("JWTの取得に失敗しました。");
+          }
+        } catch (error) {
+          console.error("JWTの取得エラー", error);
+        }
+      };
+
+      fetchJWT();
     }
-  }, [session, status, router]);
+  }, [status, session, router]);
 
-  const handleLogin = (provider: string) => async (event: React.MouseEvent) => {
+  const handleLogin = async (event: React.MouseEvent) => {
     event.preventDefault();
-    const result = await signIn(provider);
-
-    // ログインに成功したらDeckListページにリダイレクト
-    // if (result?.ok) {
-    //   router.push("/deck");
-    // } else {
-    //   // エラーハンドリング
-    //   alert(result?.error || "Login failed");
-    // }
-  };
-
-  const handleLogout = async () => {
-    await signOut({ callbackUrl: "/" });
-  };
-
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const form = event.target as HTMLFormElement;
-    const email = (form.elements.namedItem("email") as HTMLInputElement).value;
-    const password = (form.elements.namedItem("password") as HTMLInputElement)
-      .value;
-
-    if (isCreatingAccount) {
-      const response = await fetch("http://localhost:8080/api/auth/signup", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
-
-      if (response.ok) {
-        alert("Account created successfully. Please log in.");
-        setIsCreatingAccount(false);
-      } else {
-        alert("Failed to create account");
-      }
-    } else {
-      const result = await signIn("credentials", {
-        email,
-        password,
-        redirect: false,
-      });
-
-      if (result?.ok) {
-        router.push("/deck");
-      } else {
-        alert(result?.error || "Invalid credentials");
-      }
+    try {
+      // Google認証を行う
+      await signIn("google", { redirect: false });
+    } catch (error: any) {
+      alert((error as Error).message);
     }
   };
 
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <img
-          alt="Your Company"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          className="mx-auto h-10 w-auto"
-        />
         <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          {isCreatingAccount ? "Create an account" : "Sign in to your account"}
+          Sign in to your account
         </h2>
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
         <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+          <div className="mt-6 grid grid-cols-1 gap-4">
+            <a
+              href="#"
+              onClick={handleLogin}
+              className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
+            >
+              <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5">
+                <path
+                  d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
+                  fill="#EA4335"
                 />
-              </div>
-            </div>
-
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                {isCreatingAccount
-                  ? "Password (8 or more characters)"
-                  : "Password"}
-              </label>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  required
-                  autoComplete="current-password"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
+                <path
+                  d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z"
+                  fill="#4285F4"
                 />
-              </div>
-            </div>
-
-            <div className="flex items-center justify-between">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  name="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                <path
+                  d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z"
+                  fill="#FBBC05"
                 />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-3 block text-sm leading-6 text-gray-900"
-                >
-                  Remember me
-                </label>
-              </div>
-
-              {!isCreatingAccount && (
-                <div className="text-sm leading-6">
-                  <a
-                    href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot password?
-                  </a>
-                </div>
-              )}
-            </div>
-
-            <div>
-              <button
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                {isCreatingAccount ? "Create an account" : "Sign In"}
-              </button>
-            </div>
-          </form>
-
-          <div>
-            <div className="relative mt-10">
-              <div
-                aria-hidden="true"
-                className="absolute inset-0 flex items-center"
-              >
-                <div className="w-full border-t border-gray-200" />
-              </div>
-              <div className="relative flex justify-center text-sm font-medium leading-6">
-                <span className="bg-white px-6 text-gray-900">
-                  Or continue with
-                </span>
-              </div>
-            </div>
-
-            <div className="mt-6 grid grid-cols-1 gap-4">
-              <a
-                href="#"
-                onClick={handleLogin("google")}
-                className="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
-              >
-                <svg viewBox="0 0 24 24" aria-hidden="true" className="h-5 w-5">
-                  <path
-                    d="M12.0003 4.75C13.7703 4.75 15.3553 5.36002 16.6053 6.54998L20.0303 3.125C17.9502 1.19 15.2353 0 12.0003 0C7.31028 0 3.25527 2.69 1.28027 6.60998L5.27028 9.70498C6.21525 6.86002 8.87028 4.75 12.0003 4.75Z"
-                    fill="#EA4335"
-                  />
-                  <path
-                    d="M23.49 12.275C23.49 11.49 23.415 10.73 23.3 10H12V14.51H18.47C18.18 15.99 17.34 17.25 16.08 18.1L19.945 21.1C22.2 19.01 23.49 15.92 23.49 12.275Z"
-                    fill="#4285F4"
-                  />
-                  <path
-                    d="M5.26498 14.2949C5.02498 13.5699 4.88501 12.7999 4.88501 11.9999C4.88501 11.1999 5.01998 10.4299 5.26498 9.7049L1.275 6.60986C0.46 8.22986 0 10.0599 0 11.9999C0 13.9399 0.46 15.7699 1.28 17.3899L5.26498 14.2949Z"
-                    fill="#FBBC05"
-                  />
-                  <path
-                    d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z"
-                    fill="#34A853"
-                  />
-                </svg>
-                <span className="text-sm font-semibold leading-6">Google</span>
-              </a>
-            </div>
+                <path
+                  d="M12.0004 24.0001C15.2404 24.0001 17.9654 22.935 19.9454 21.095L16.0804 18.095C15.0054 18.82 13.6204 19.245 12.0004 19.245C8.8704 19.245 6.21537 17.135 5.2654 14.29L1.27539 17.385C3.25539 21.31 7.3104 24.0001 12.0004 24.0001Z"
+                  fill="#34A853"
+                />
+              </svg>
+              <span className="text-sm font-semibold leading-6">Google</span>
+            </a>
           </div>
         </div>
-
-        <p className="mt-10 text-center text-sm text-gray-500">
-          {isCreatingAccount ? (
-            <a
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              href="#"
-              onClick={() => setIsCreatingAccount(false)}
-            >
-              Sign in to your account
-            </a>
-          ) : (
-            <>
-              Not registered?{" "}
-              <a
-                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-                href="#"
-                onClick={() => setIsCreatingAccount(true)}
-              >
-                Create an account
-              </a>
-            </>
-          )}
-        </p>
       </div>
     </div>
   );
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,86 +1,12 @@
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
 
 export default NextAuth({
   debug: true,
-  session: { strategy: "database" },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      async profile(profile) {
-        const res = await fetch("http://localhost:8080/api/auth/google-login", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            email: profile.email,
-            googleId: profile.sub,
-          }),
-          credentials: "include",
-        });
-
-        const user = await res.json();
-        return user ? user : null;
-      },
-    }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "Email" },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Password",
-        },
-      },
-      async authorize(credentials) {
-        try {
-          const res = await fetch("http://localhost:8080/api/auth/login", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(credentials),
-            credentials: "include",
-          });
-
-          if (!res.ok) {
-            throw new Error("Invalid credentials");
-          }
-
-          const user = await res.json();
-
-          if (user) {
-            return user;
-          } else {
-            return null;
-          }
-        } catch (error) {
-          console.error("Error during authentication:", error);
-          return null;
-        }
-      },
     }),
   ],
-  callbacks: {
-    session: ({ session, token }) => {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          role: token.role,
-        },
-      };
-    },
-    redirect: async ({ url, baseUrl }) => {
-      if (url === "/") {
-        return baseUrl + "/deck";
-      }
-      return baseUrl;
-    },
-  },
 });
