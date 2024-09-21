5e6ff5ba5442ac4061aeed1d15e31e358f81e5f9
kohei240155 <k.pino519@gmail.com>
2024-09-22 08:43:25 +0900

    カラーピッカーの表示位置を修正

--- features/routes/userSettings/components/UserSettingsForm.tsx
+++ features/routes/userSettings/components/UserSettingsForm.tsx
@@ -222,7 +222,7 @@ const UserSettingsForm = () => {
           <div className="flex justify-between mb-2">
             <button
               type="button"
-              onClick={() => router.push("/decks")}
+              onClick={() => router.push("/deck")}
               className="w-1/2 mr-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
               Backward
@@ -239,8 +239,8 @@ const UserSettingsForm = () => {
               style={{
                 position: "absolute",
                 zIndex: 2,
-                top: "100%",
-                left: 0,
+                top: "150px",
+                left: "20px",
               }}
             >
               <div
