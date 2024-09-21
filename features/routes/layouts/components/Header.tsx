1490dedca6a97b343bad604138d725f61a498e62
kohei240155 <k.pino519@gmail.com>
2024-09-22 01:11:34 +0900

    要素を左揃えに修正

--- features/routes/layouts/components/Header.tsx
+++ features/routes/layouts/components/Header.tsx
@@ -114,7 +114,7 @@ const Header = () => {
               className={`${isMenuOpen ? "block" : "hidden"} w-full md:block md:w-auto z-50`}
               id="navbar-multi-level"
             >
-              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 items-center">
+              <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 items-start text-left">
                 <li>
                   <Link
                     href="/"
