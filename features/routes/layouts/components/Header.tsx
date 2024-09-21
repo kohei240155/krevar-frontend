8e9cc18ac2544a5f4404033ec262be5ed0f47474
kohei240155 <k.pino519@gmail.com>
2024-09-22 01:12:29 +0900

    アイコンの表示を修正

--- features/routes/layouts/components/Header.tsx
+++ features/routes/layouts/components/Header.tsx
@@ -131,7 +131,7 @@ const Header = () => {
                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-200 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     onClick={handleMenuClick}
                   >
-                    <MdOutlineLibraryAdd className="inline-block mr-2 md:hidden" />
+                    <MdOutlineLibraryAdd className="inline-block mr-2" />
                     Add Deck
                   </Link>
                 </li>
