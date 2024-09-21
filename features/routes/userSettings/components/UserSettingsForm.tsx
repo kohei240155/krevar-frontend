0e659326ead8992ea5060fce61b94d7f9146bc45
kohei240155 <k.pino519@gmail.com>
2024-09-21 21:31:54 +0900

    レスポンシブ対応

--- features/routes/userSettings/components/UserSettingsForm.tsx
+++ features/routes/userSettings/components/UserSettingsForm.tsx
@@ -82,149 +82,154 @@ const UserSettingsForm = () => {
   }
 
   return (
-    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md relative">
-      <h1 className="text-2xl font-bold text-left mb-6">User Settings</h1>
-      <form>
-        <div className="mb-5">
-          <label className="block text-sm font-medium text-gray-700">
-            User Name:
-          </label>
-          <input
-            type="text"
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div className="mb-5">
-          <label className="block text-sm font-medium text-gray-700">
-            Native Language:
-          </label>
-          <Listbox value={nativeLanguageId} onChange={setNativeLanguageId}>
-            <div className="relative mt-2">
-              <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                <span className="block truncate">
-                  {getLanguageName(nativeLanguageId)}
-                </span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                  <ChevronUpDownIcon
-                    aria-hidden="true"
-                    className="h-5 w-5 text-gray-400"
-                  />
-                </span>
-              </ListboxButton>
-              <ListboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {languageList.map((language) => (
-                  <ListboxOption
-                    key={language.id}
-                    value={language.id}
-                    className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
-                  >
-                    <span className="block truncate font-normal group-data-[selected]:font-semibold">
-                      {language.languageName}
-                    </span>
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
-                      <CheckIcon aria-hidden="true" className="h-5 w-5" />
-                    </span>
-                  </ListboxOption>
-                ))}
-              </ListboxOptions>
-            </div>
-          </Listbox>
-        </div>
-        <div className="mb-5">
-          <label className="block text-sm font-medium text-gray-700">
-            Learning Language:
-          </label>
-          <Listbox value={learningLanguageId} onChange={setLearningLanguageId}>
-            <div className="relative mt-2">
-              <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                <span className="block truncate">
-                  {getLanguageName(learningLanguageId)}
-                </span>
-                <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                  <ChevronUpDownIcon
-                    aria-hidden="true"
-                    className="h-5 w-5 text-gray-400"
-                  />
-                </span>
-              </ListboxButton>
-              <ListboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {languageList.map((language) => (
-                  <ListboxOption
-                    key={language.id}
-                    value={language.id}
-                    className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
-                  >
-                    <span className="block truncate font-normal group-data-[selected]:font-semibold">
-                      {language.languageName}
-                    </span>
-                    <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
-                      <CheckIcon aria-hidden="true" className="h-5 w-5" />
-                    </span>
-                  </ListboxOption>
-                ))}
-              </ListboxOptions>
-            </div>
-          </Listbox>
-        </div>
-        <div className="mb-5">
-          <label
-            htmlFor="subscriptionStatus"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Subscription Status:
-          </label>
-          <input
-            type="text"
-            id="subscriptionStatus"
-            value={subscriptionStatus}
-            readOnly
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
-          />
-          <button
-            type="button"
-            // onClick={() => onSubscriptionUpdate(subscriptionStatus)}
-            className="mt-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Update Subscription
-          </button>
-        </div>
-        <div className="mb-5">
-          <div
-            onClick={() => setDisplayColorPicker(!displayColorPicker)}
-            className="inline-flex items-center justify-center px-2 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-            style={{
-              backgroundColor: highlightColor,
-              cursor: "pointer",
-              width: "30px",
-              height: "30px",
-            }}
-          />
-          <input
-            type="color"
-            id="highlightColor"
-            value={highlightColor}
-            onChange={(e) => setHighlightColor(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
-          />
-        </div>
-        <div className="flex justify-between mb-2">
-          <button
-            type="button"
-            onClick={() => router.push("/settings")}
-            className="w-1/2 mr-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Backward
-          </button>
-          <button
-            type="submit"
-            className="w-1/2 ml-2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Save
-          </button>
-        </div>
-      </form>
+    <div className="relative p-5">
+      <div className="max-w-md mx-auto p-6 bg-white rounded-lg shadow-md relative">
+        <h1 className="text-2xl font-bold text-left mb-6">User Settings</h1>
+        <form>
+          <div className="mb-5">
+            <label className="block text-sm font-medium text-gray-700">
+              User Name:
+            </label>
+            <input
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="mb-5">
+            <label className="block text-sm font-medium text-gray-700">
+              Native Language:
+            </label>
+            <Listbox value={nativeLanguageId} onChange={setNativeLanguageId}>
+              <div className="relative mt-2">
+                <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <span className="block truncate">
+                    {getLanguageName(nativeLanguageId)}
+                  </span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon
+                      aria-hidden="true"
+                      className="h-5 w-5 text-gray-400"
+                    />
+                  </span>
+                </ListboxButton>
+                <ListboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {languageList.map((language) => (
+                    <ListboxOption
+                      key={language.id}
+                      value={language.id}
+                      className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+                    >
+                      <span className="block truncate font-normal group-data-[selected]:font-semibold">
+                        {language.languageName}
+                      </span>
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                        <CheckIcon aria-hidden="true" className="h-5 w-5" />
+                      </span>
+                    </ListboxOption>
+                  ))}
+                </ListboxOptions>
+              </div>
+            </Listbox>
+          </div>
+          <div className="mb-5">
+            <label className="block text-sm font-medium text-gray-700">
+              Learning Language:
+            </label>
+            <Listbox
+              value={learningLanguageId}
+              onChange={setLearningLanguageId}
+            >
+              <div className="relative mt-2">
+                <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                  <span className="block truncate">
+                    {getLanguageName(learningLanguageId)}
+                  </span>
+                  <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                    <ChevronUpDownIcon
+                      aria-hidden="true"
+                      className="h-5 w-5 text-gray-400"
+                    />
+                  </span>
+                </ListboxButton>
+                <ListboxOptions className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                  {languageList.map((language) => (
+                    <ListboxOption
+                      key={language.id}
+                      value={language.id}
+                      className="group relative cursor-default select-none py-2 pl-8 pr-4 text-gray-900 data-[focus]:bg-indigo-600 data-[focus]:text-white"
+                    >
+                      <span className="block truncate font-normal group-data-[selected]:font-semibold">
+                        {language.languageName}
+                      </span>
+                      <span className="absolute inset-y-0 left-0 flex items-center pl-1.5 text-indigo-600 group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                        <CheckIcon aria-hidden="true" className="h-5 w-5" />
+                      </span>
+                    </ListboxOption>
+                  ))}
+                </ListboxOptions>
+              </div>
+            </Listbox>
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="subscriptionStatus"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Subscription Status:
+            </label>
+            <input
+              type="text"
+              id="subscriptionStatus"
+              value={subscriptionStatus}
+              readOnly
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
+            />
+            <button
+              type="button"
+              // onClick={() => onSubscriptionUpdate(subscriptionStatus)}
+              className="mt-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Update Subscription
+            </button>
+          </div>
+          <div className="mb-5">
+            <div
+              onClick={() => setDisplayColorPicker(!displayColorPicker)}
+              className="inline-flex items-center justify-center px-2 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              style={{
+                backgroundColor: highlightColor,
+                cursor: "pointer",
+                width: "30px",
+                height: "30px",
+              }}
+            />
+            <input
+              type="color"
+              id="highlightColor"
+              value={highlightColor}
+              onChange={(e) => setHighlightColor(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:border-indigo-500 sm:text-sm"
+            />
+          </div>
+          <div className="flex justify-between mb-2">
+            <button
+              type="button"
+              onClick={() => router.push("/settings")}
+              className="w-1/2 mr-2 inline-flex items-center justify-center px-4 py-2 border border-indigo-600 text-sm font-medium rounded-md text-indigo-600 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Backward
+            </button>
+            <button
+              type="submit"
+              className="w-1/2 ml-2 inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
