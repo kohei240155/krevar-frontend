71422ec1aa3e1cc66669d81685f5cb63c4abc1e1
kohei240155 <k.pino519@gmail.com>
2024-09-27 23:35:16 +0900

    レイアウト修正

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import "tailwindcss/tailwind.css";
 
 const SubscribeButton = ({ selectedPlan }: { selectedPlan: string }) => {
   const handleSubscription = async () => {
