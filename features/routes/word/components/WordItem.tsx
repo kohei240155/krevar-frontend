3926b11de0f23c1d07c08f447f43881b049c3b9f
kohei240155 <k.pino519@gmail.com>
2024-10-28 20:33:03 +0900

    単語表示時の表示条件を修正

--- features/routes/word/components/WordItem.tsx
+++ features/routes/word/components/WordItem.tsx
@@ -6,33 +6,39 @@ export interface WordItemProps {
   deckId: string;
 }
 
-const WordItem: React.FC<WordItemProps> = ({ word, deckId }) => (
-  <li
-    className="relative flex flex-row justify-between items-center p-3 pr-4 bg-white rounded-lg shadow-md space-x-4 word-list-item"
-    style={{
-      height: "auto",
-      minHeight: "120px",
-      position: "relative",
-      border: "1px solid #e0e0e0",
-    }}
-  >
-    <div
-      className="absolute left-0 w-1 bg-gray-500"
+const WordItem: React.FC<WordItemProps> = ({ word, deckId }) => {
+  const extractHighlightedText = (text: string) => {
+    const match = text.match(/<span.*?>(.*?)<\/span>/);
+    return match ? match[1] : text;
+  };
+
+  return (
+    <li
+      className="relative flex flex-row justify-between items-center p-3 pr-4 bg-white rounded-lg shadow-md space-x-4 word-list-item"
       style={{
-        height: "95%",
-        top: "50%",
-        transform: "translateY(-50%)",
+        height: "auto",
+        minHeight: "120px",
+        position: "relative",
+        border: "1px solid #e0e0e0",
       }}
-    ></div>
-    <div className="flex flex-col">
-      <span
-        className="text-lg font-medium"
-        dangerouslySetInnerHTML={{ __html: word.originalText }}
-      />
-      <span className="text-gray-600">{word.translatedText}</span>
-    </div>
-    <WordSettingsButton wordId={word.id.toString()} deckId={deckId} />
-  </li>
-);
+    >
+      <div
+        className="absolute left-0 w-1 bg-gray-500"
+        style={{
+          height: "95%",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      ></div>
+      <div className="flex flex-col">
+        <span className="text-lg font-medium">
+          {extractHighlightedText(word.originalText)}
+        </span>
+        <span className="text-gray-600">{word.translatedText}</span>
+      </div>
+      <WordSettingsButton wordId={word.id.toString()} deckId={deckId} />
+    </li>
+  );
+};
 
 export default WordItem;
