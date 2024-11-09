a96862f1f42bf1ef4f45e1b710b6f59370f479f9
kohei240155 <k.pino519@gmail.com>
2024-11-09 16:57:06 +0900

    文字のレイアウトを修正

--- features/routes/layouts/components/StatsSection.tsx
+++ features/routes/layouts/components/StatsSection.tsx
@@ -2,43 +2,43 @@
 const StatsSection: React.FC = () => {
   return (
     <section className="bg-gray-100 p-3">
-      <h3 className="text-3xl font-semibold text-gray-800 mb-4 text-center">
+      <h3 className="text-4xl font-semibold text-gray-800 mb-8 text-center">
         KREVARはこのような方に最適です
       </h3>
       <ul className="list-none text-gray-700 flex flex-col items-center">
-        <li className="flex items-start mb-2 w-full max-w-2xl">
+        <li className="flex items-start mb-5 w-full max-w-3xl">
           <span className="text-gray-500 text-2xl mr-2">✔️</span>
-          <span className="text-xl">
+          <span className="text-2xl">
             単語帳で暗記したけど、
-            <span className="text-gray-900 text-xl font-bold">
+            <span className="text-gray-900 text-2xl font-bold">
               単調で苦痛に感じた人
             </span>
           </span>
         </li>
-        <li className="flex items-start mb-2 w-full max-w-2xl">
+        <li className="flex items-start mb-5 w-full max-w-3xl">
           <span className="text-gray-500 text-2xl mr-2">✔️</span>
-          <span className="text-xl">
+          <span className="text-2xl">
             話題のイマージョンラーニングを始たけど、
-            <span className="text-gray-900 text-xl font-bold">
+            <span className="text-gray-900 text-2xl font-bold">
               面倒で続かなかった人
             </span>
           </span>
         </li>
-        <li className="flex items-start mb-2 w-full max-w-2xl">
+        <li className="flex items-start mb-5 w-full max-w-3xl">
           <span className="text-gray-500 text-2xl mr-2">✔️</span>
-          <span className="text-xl">
+          <span className="text-2xl">
             単語を復習する間隔がわからず、
-            <span className="text-gray-900 text-xl font-bold">
+            <span className="text-gray-900 text-2xl font-bold">
               なかなか単語を覚えられない人
             </span>
           </span>
         </li>
-        <li className="flex items-start w-full max-w-2xl">
+        <li className="flex items-start mb-5 w-full max-w-3xl">
           <span className="text-gray-500 text-2xl mr-2">✔️</span>
-          <span className="text-xl">
+          <span className="text-2xl">
             英語以外の単語も覚えたいけど、
-            <span className="text-gray-900 text-xl font-bold">
-              いい単語帳に出会えていない人
+            <span className="text-gray-900 text-2xl font-bold">
+              単語学習がはかどっていない人
             </span>
           </span>
         </li>
