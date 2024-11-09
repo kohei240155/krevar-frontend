e9d7b2030344f22b424dc91a24e618b31ad96154
kohei240155 <k.pino519@gmail.com>
2024-11-09 19:46:25 +0900

    文言を修正

--- features/routes/layouts/components/StatsSection.tsx
+++ features/routes/layouts/components/StatsSection.tsx
@@ -3,13 +3,13 @@ const StatsSection: React.FC = () => {
   return (
     <section className="bg-gray-100 p-3">
       <h3 className="text-4xl font-semibold text-gray-800 mb-10 text-center">
-        KREVARはこのような方に最適です
+        こんなお悩みありませんか？
       </h3>
       <ul className="list-none text-gray-700 flex flex-col items-center">
         <li className="flex items-start mb-6 w-full max-w-3xl">
           <span className="text-gray-500 text-2xl mr-2">✔️</span>
           <span className="text-2xl">
-            単語帳で暗記したけど、
+            単語帳を使っていたけど、
             <span className="text-gray-900 text-2xl font-bold">
               単調で苦痛に感じた人
             </span>
@@ -38,7 +38,7 @@ const StatsSection: React.FC = () => {
           <span className="text-2xl">
             英語以外の言語を学んでいるけど、
             <span className="text-gray-900 text-2xl font-bold">
-              単語学習がはかどっていない人
+              いい教材に出会えてない人
             </span>
           </span>
         </li>
