bb7b9969bb72e1a81e498f4804e78ed617125030
kohei240155 <k.pino519@gmail.com>
2024-12-30 22:58:01 +0900

    コメントを追加

--- app/deck/add/page.tsx
+++ app/deck/add/page.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import DeckForm from "../../../features/routes/deck/components/DeckForm";
 
+/**
+ * デッキ追加ページ
+ * */
 const DeckAddPage = () => {
   return <DeckForm isEditMode={false} />;
 };
